6c361cd90e79cb84980dc55a7e60a4c04b9727cf
jaei <jae2son@gmail.com>
2023-08-04 21:15:18 +0900

    제발

---
+++ src/main/java/org/example/myaddTest7/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.myaddTest7;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "main-menu";
+    }
+}
